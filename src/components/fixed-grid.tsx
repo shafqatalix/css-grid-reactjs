2d68ccfef6511d466edf456ed10167c5192b02c3
Xalisys <xalisys@hotmail.com>
2020-06-05 22:52:24 -0400

    simplify fixed-grid api

--- src/components/fixed-grid.tsx
+++ src/components/fixed-grid.tsx
@@ -4,15 +4,15 @@ import { GridProps } from "./types";
 import SimpleBar from "simplebar-react";
 import { Grid } from "./grid";
 
-type FixedGridProps = GridProps & { autoHide?: boolean; scrollbarStyles?: CSSProperties };
+type FixedGridProps = GridProps & { autoHideScrollbar?: boolean; scrollbarStyles?: CSSProperties };
 /**
  * Fixed size grid shows crossbars incase of content overflow.
  */
 export const FixedGrid = (props: FixedGridProps) => {
-    const { children, scrollbarStyles, autoHide, ...rest } = props;
+    const { children, scrollbarStyles, autoHideScrollbar = true, ...rest } = props;
 
     return (
-        <SimpleBar style={scrollbarStyles} autoHide={autoHide}>
+        <SimpleBar style={scrollbarStyles} autoHide={autoHideScrollbar}>
             <div>
                 <Grid {...rest}>{children}</Grid>
             </div>
