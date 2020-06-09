1cf22f00008016ecda2b5196e4a119d9874847f6
Xalisys <xalisys@hotmail.com>
2020-06-08 22:34:29 -0400

    updated jest

--- jest.config.js
+++ jest.config.js
@@ -1,14 +1,6 @@
+/** @typedef {import('ts-jest')} */
+/** @type {import('@jest/types').Config.InitialOptions} */
 module.exports = {
-  rootDir: "./",
-  moduleNameMapper: {
-    "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2)$":
-      "identity-obj-proxy",
-    "\\.(css|less)$": "identity-obj-proxy",
-  },
-  transform: {
-    "^.+\\.(ts|tsx)$": "<rootDir>/jtsp.js",
-  },
-  moduleFileExtensions: ["js", "ts", "tsx"],
-  testMatch: ["**/*.test.(ts|tsx|js)"],
-  testURL: "https://npmjs.com/",
+    // rootDir: "./",
+    preset: "ts-jest",
 };
